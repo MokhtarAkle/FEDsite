a7b6a0980435f0bcb1ac5d033c8dcc2855fcba9c
MokhtarAkle <45001009+MokhtarAkle@users.noreply.github.com>
2021-12-03 10:30:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
 // JavaScript Document
+var menuDisplay = document.querySelector(".hamMenu");
+var hamClick = document.querySelector(".menuImage");
+var menuHider = document.querySelector(".hamMenu1");
+
+function displayHam() {
+        menuDisplay.classList.remove("hamMenu");
+        menuDisplay.classList.add("hamMenu1");
+        
+}
+
+
+
+hamClick.addEventListener("click", displayHam);
+function clickAway(){
+    if (menuDisplay.classList.contains("hamMenu1")){
+        document.addEventListener("click", function(event){
+            var clickCheck = menuHider.contains(event.target);
+            if (!clickCheck){
+                menuDisplay.classList.remove("hamMenu1");
+                menuDisplay.classList.add("hamMenu");
+                console.log(clickAway());
+            }
+        })
+    }
+}
+
+
